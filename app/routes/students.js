918177727c091f4740a32be36131f1be6845bd7b
YusukeOkahata <yusukeyusukesmile@outlook.jp>
2024-06-17 00:01:15 +0900

    フロントエンド復旧しました

---
+++ app/routes/students.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var router = express.Router();
+
+const pool = require("../mysqlConnection");
+
+/* GET users listing. */
+router.get("/", (req, res, next) => {
+  pool.query("SELECT * from users;", (err, results, fields) => {
+    if (err) {
+      console.error("students.js: sql execute error");
+    } else {
+      console.log("students.js: sql execute success");
+    }
+    pool.end();
+    //res.send(results);
+  });
+
+  res.render("students");
+});
+
+module.exports = router;
