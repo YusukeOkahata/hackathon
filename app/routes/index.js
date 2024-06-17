d54c85805ba18597d6df57eb3863293c178b023c
Imonao <n.k0831@softbank.ne.jp>
2024-06-17 10:58:03 +0900

    ieee!

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,3 +1,4 @@
+//ルーティングされている
 var express = require('express');
 var router = express.Router();
 
---
+++ mysql/init/insert_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO `users` (name, email, password) VALUES("taro", 'hoge@fuga.com', 'pass123');
+INSERT INTO `users` (name) VALUES("sakabe");
+INSERT INTO `users` (name) VALUES("kentaro");
