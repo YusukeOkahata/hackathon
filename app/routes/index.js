d58b1eac59d6bff08f647f6f5cacf58d93c99fcb
YusukeOkahata <yusukeyusukesmile@outlook.jp>
2024-06-18 02:02:24 +0900

    変なやつ消した

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var router = express.Router();
-const bcrypt = require("bcrypt");
 const pool = require("../mysqlConnection");
 
 /* GET users listing. */
@@ -22,33 +21,10 @@ router.get("/", (req, res, next) => {
 router.post("/", (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
-  //console.log(`username:${username}`);
-  //console.log(`pass:${password}`);
+  console.log(`username:${username}`);
+  console.log(`pass:${password}`);
 
-  if (username === "Onoteacher" && password === "ice_number1") {
-    // 特定のユーザー名とパスワードの場合
-    res.render("teacher");
-  } else {
-    const sql = "SELECT * FROM users WHERE username = ?";
-    pool.query(sql, [username], (err, result) => {
-      if (err) throw err;
-
-      if (result.length === 0) {
-        res.send("No such user found");
-      } else {
-        bcrypt.compare(password, result[0].password, (err, isMatch) => {
-          if (err) throw err;
-
-          if (isMatch) {
-            res.render("students");
-          } else {
-            res.send("Invalid credentials");
-          }
-        });
-      }
-    });
-  }
-  //res.render("students");
+  res.render("students");
 });
 
 module.exports = router;
