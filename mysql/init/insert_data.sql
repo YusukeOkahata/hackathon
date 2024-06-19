e7f6b6b2f5a2406f0835fccccd12ce901d049e7d
YusukeOkahata <yusukeyusukesmile@outlook.jp>
2024-06-19 10:39:00 +0900

    changed sql

--- mysql/init/insert_data.sql
+++ mysql/init/insert_data.sql
@@ -3,9 +3,10 @@ INSERT INTO `users` (username, password) VALUES("jolly",  'yusuke');
 INSERT INTO `users` (username, password) VALUES("kuramuki",  'shoma');
 
 
-INSERT INTO `questions` (question_by,content,status) VALUES("okahata","こんにちは","未返信");
-INSERT INTO `questions` (question_by,content,status) VALUES("kadoya","おはようございます","返信済");
-INSERT INTO `questions` (question_by,content,status) VALUES("okahata","質問があります","未返信");
-INSERT INTO `questions` (question_by,content,status) VALUES("kadoya","解決しました","未返信");
-INSERT INTO `questions` (question_by,content,status) VALUES("okahata","研究について","未返信");
-INSERT INTO `questions` (question_by,content,status) VALUES("okahata","解決しました","未返信");
+
+INSERT INTO `questions` (question_by,content,status) VALUES("2","こんにちは","未返信");
+INSERT INTO `questions` (question_by,content,status) VALUES("3","おはようございます","返信済");
+INSERT INTO `questions` (question_by,content,status) VALUES("2","質問があります","未返信");
+INSERT INTO `questions` (question_by,content,status) VALUES("3","解決しました","未返信");
+INSERT INTO `questions` (question_by,content,status) VALUES("2","研究について","未返信");
+INSERT INTO `questions` (question_by,content,status) VALUES("2","解決しました","未返信");
