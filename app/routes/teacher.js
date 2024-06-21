b2e8d9566b30bcff9f6c6e847764afd2ca2fda92
YusukeOkahata <yusukeyusukesmile@outlook.jp>
2024-06-21 21:11:40 +0900

    サイドバーを標準時で閉じた状態にしました

--- app/routes/teacher.js
+++ app/routes/teacher.js
@@ -65,7 +65,8 @@ router.get("/", (req, res) => {
                 return res.status(500).send("Database error");
               }
 
-              console.log(userList);
+              //console.log(userList);
+              //console.log(messages);
               res.render("teacher", {
                 username: username,
                 messages: messages,
@@ -83,32 +84,6 @@ router.get("/", (req, res) => {
     }
   );
 });
-
-/* POST a new message */
-/*
-router.post("/message", (req, res, next) => {
-  if (!req.session.username) {
-    res.status(403).send("Unauthorized");
-    return;
-  }
-
-  const { message } = req.body;
-  const username = req.session.username;
-  pool.query(
-    "INSERT INTO messages (user_id, message, sender) VALUES (?, ?, ?)",
-    [username, message, "teacher"],
-    (err, results) => {
-      if (err) {
-        console.error("teacher.js: sql insert error");
-        res.status(500).send("Database insert error");
-      } else {
-        res.status(200).send("Message saved");
-      }
-    }
-  );
-});
-//
-*/
 
 // メッセージ送信
 router.post("/send", (req, res) => {
